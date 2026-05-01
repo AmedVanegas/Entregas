01f80a233bb9fdaadc0ae4899160d6433602c628
Amed Vanegas <amedvanegas368@gmail.com>
2026-04-30 21:29:24 -0500

    carrito de compras

--- Trabajo en clase/2.__Ejercicios_27_abril/api.js
+++ Trabajo en clase/2.__Ejercicios_27_abril/api.js
@@ -1,28 +1,24 @@
 const API = "https://fakeapi.net/products";
 
-
-function suma (data) {
-    let total = 0
+function suma(data) {
+  let total = 0;
   data.forEach(function (value) {
-    total += value.price;
-    
+    total += value.stock;
   });
 
-  console.log(`el total es ${total}`)
+  return total;
 }
 
 async function fetchapi() {
   try {
     const response = await fetch(API);
     const data = await response.json();
+    const total = suma(data.data).toFixed(2)
 
-    suma(data.data)
-
+    console.log(`el valor total es de ${total}`)
   } catch (error) {
     console.error("Error al conectarse");
   }
 }
 
-fetchapi()
-  
-
+fetchapi();
